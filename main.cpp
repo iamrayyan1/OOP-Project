6be3498ad7410c71d8ac3f0cfface2fba1080f64
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2024-04-29 09:56:24 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+#include<time.h>
+
+// Directories
+#include "MainHeader.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	
+	getMenu();
+	
+	
+}
